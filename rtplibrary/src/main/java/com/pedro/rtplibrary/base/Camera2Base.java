35885f1c3fc7cef30f24380763261021d878e97e
Dan Darolti <78900979+dandaroltiEN@users.noreply.github.com>
2022-04-28 13:49:39 +0300

    expose camera features add jitpack.io dependencies

--- rtplibrary/src/main/java/com/pedro/rtplibrary/base/Camera2Base.java
+++ rtplibrary/src/main/java/com/pedro/rtplibrary/base/Camera2Base.java
@@ -144,6 +144,10 @@ public abstract class Camera2Base implements GetAacData, GetVideoData, GetMicrop
     init(context);
   }
 
+  public Camera2ApiManager getCameraManager() {
+    return cameraManager;
+  }
+
   private void init(Context context) {
     cameraManager = new Camera2ApiManager(context);
     videoEncoder = new VideoEncoder(this);
